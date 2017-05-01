c5ff60735d314b59a35d4c4408d769d31400b981
Kareem Waleed <kareemwaleed31@gmail.com>
2017-05-01 20:35:52 +0200

    Adds IfStatement rule

---
+++ src/phaseTwo/Matched.java
@@ -0,0 +1,11 @@
+package phaseTwo;
+
+public class Matched implements IfStatement {
+
+	@Override
+	public String getValue(){
+	
+		return "";
+	}
+
+}
---
+++ src/phaseTwo/UnMatched.java
@@ -0,0 +1,11 @@
+package phaseTwo;
+
+public class UnMatched implements IfStatement {
+
+	@Override
+	public String getValue() {
+		
+		return "";
+	}
+
+}
