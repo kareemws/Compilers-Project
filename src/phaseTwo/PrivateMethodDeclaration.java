5f06c84e2a7219da5c433855d929ff4d77390271
abdallahEid <abdallaheidm@gmail.com>
2017-05-01 21:27:40 +0200

    fixing MethodDeclaration

--- src/phaseTwo/PrivateMethodDeclaration.java
+++ src/phaseTwo/PrivateMethodDeclaration.java
@@ -13,40 +13,41 @@ public class PrivateMethodDeclaration implements MethodDeclaration{
 	ArrayList<Statement> statements = new ArrayList<>();;
 	Expression expression ;
 	
-	public PrivateMethodDeclaration(Type type, Identifier identifier, VarDeclaration varDeclarations, Statement ArrayList<Statement> statements, Expression expression){
+	public PrivateMethodDeclaration(Type type, Identifier identifier, ArrayList<VarDeclaration> varDeclarations, ArrayList<Statement> statements, Expression expression){
 		this.type = type ;
 		this.identifier = identifier;
 		this.types = null ;
 		this.identifiers = null ;
-		this.varDeclaration = VarDeclaration ;
-		this.statement = statement ;
+		this.varDeclarations = varDeclarations ;
+		this.statements = statements ;
 		this.expression = expression;
 	}
 	
 	public PrivateMethodDeclaration(Type type, Identifier identifier, ArrayList<VarDeclaration> varDeclarations, ArrayList<Statement> statements, Expression expression, Type type2, Identifier identifier2 ){
 		this.type = type ;
 		this.identifier = identifier;
-		this.types.add(type2) ;
-		this.identifiers.add(identifier2) ;
-		this.varDeclaration = VarDeclaration ;
-		this.statement = statement ;
+		this.types = null ;
+		this.identifiers = null ;
+		this.varDeclarations = varDeclarations ;
+		this.statements = statements ;
 		this.expression = expression;
 	}
 	
-	public PrivateMethodDeclaration(Type type, Identifier identifier, VarDeclaration varDeclarations, ArrayList<Statement> statements, Expression expression, ArrayList<Type> types ,ArrayList<Identifier> identifiers ){
+	public PrivateMethodDeclaration(Type type, Identifier identifier, ArrayList<VarDeclaration> varDeclarations, ArrayList<Statement> statements, Expression expression, ArrayList<Type> types ,ArrayList<Identifier> identifiers ){
 		this.type = type ;
 		this.identifier = identifier;
-		this.types = types ;
-		this.identifiers = identifiers ;
-		this.varDeclaration = VarDeclaration ;
-		this.statement = statement ;
+		this.types = null ;
+		this.identifiers = null ;
+		this.varDeclarations = varDeclarations ;
+		this.statements = statements ;
 		this.expression = expression;
 	}
 	
+	@Override
 	public String getValue(){
 		String result = " private " + type.getValue() + " " + identifier.getValue() + " ( " ;
 		for(int i=0 ; i < types.size() ; i++){
-			if(!i)
+			if(i == 0)
 				result += types.get(i).getValue() + " " + identifiers.get(i).getValue() + " " ;
 			else
 				result += " , " + types.get(i).getValue() + " " + identifiers.get(i).getValue() + " " ;
@@ -56,7 +57,7 @@ public class PrivateMethodDeclaration implements MethodDeclaration{
 			result += varDeclarations.get(i).getValue() + " " ;
 		for(int i=0 ; i < statements.size() ; i++)
 			result += statements.get(i).getValue() + " " ;
-		result += " return " + expression.getValue() + " ; } "
+		result += " return " + expression.getValue() + " ; } " ;
 	}
 	
 	// end class
