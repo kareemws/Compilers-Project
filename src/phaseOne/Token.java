3f7be0d7d9b5da92d0ae3cef9e331ff74df07fa4
abdallahEid <abdallaheidm@gmail.com>
2017-05-02 14:57:35 +0200

    Adds half token class

--- src/phaseOne/Token.java
+++ src/phaseOne/Token.java
@@ -2,4 +2,36 @@ package phaseOne;
 
 public class Token {
 	public final static String COMMENT2 = "<COMMENT2>";
+	
+	// Data Types
+	
+	public final static String ID = "<ID>" ;
+	public final static String BOOLEAN = "<BOOLEAN>" ;
+	public final static String CHARACTER = "<CHARACTER>" ;
+	public final static String FLOAT = "<FLOAT>" ;
+	public final static String STRING = "<STRING>" ;
+	public final static String INT = "<INT>" ;
+	
+	//Keywords
+	public final static String IF = "<IF>" ;
+	public final static String ELSE = "<ELSE>" ;
+	public final static String MAIN = "<MAIN>" ;
+	public final static String THIS = "<THIS>" ;
+	public final static String TRUE = "<TRUE>" ;
+	public final static String VOID = "<VOID>" ;
+	public final static String CLASS = "<CLASS>" ;
+	public final static String FALSE = "<FALSE>" ;
+	public final static String WHILE = "<WHILE>" ;
+	public final static String LENGTH = "<LENGTH>" ;
+	public final static String PUBLIC = "<PUBLIC>" ;
+	public final static String PRIVATE = "<PRIVATE>" ;
+	public final static String RETURN = "<RETURN>" ;
+	public final static String STATIC = "<STATIC>" ;
+	public final static String NEW = "<NEW>" ;
+	public final static String EXTENDS = "<EXTENDS>" ;
+	public final static String SYSTEM_OUT_PRINTLN = "<SYSTEM.OUT.PRINTLN>" ;
+	public final static String DOT = "<DOT>" ;
+	
+
+	
 }
