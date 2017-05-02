c73d3a09e567f208cdc4c2876acb5c160f10a861
Kareem Waleed <kareemwaleed31@gmail.com>
2017-05-02 12:22:33 +0200

    Adds Parser class

---
+++ src/phaseOne/Parser.java
@@ -0,0 +1,13 @@
+package phaseOne;
+
+import java.util.Queue;
+
+public class Parser {
+
+	private Queue<Lexeme> lexemes;
+	
+	public Parser(Queue<Lexeme> lexemes){
+		this.lexemes = lexemes;
+	}
+	
+}
